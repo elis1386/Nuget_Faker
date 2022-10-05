604e921ffde6b72f226892944639b7fd4407cab1
Pashkovskaya Elizaveta <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-10-05 14:33:16 +0300

    added nugetexample project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Faker;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var name = Name.FullName();
+        var city = Address.City();
+        var address = Address.StreetAddress();
+        var random = new Random();
+        var letter = Convert.ToChar(random.Next('A', 'Z'));
+        var tunnus = RandomNumber.Next(0, 100000000);
+        Console.WriteLine($"Name: {name}; Osoite: {city} {address}; Henkilötunnus: {letter}{tunnus}.");
+    }
+}
